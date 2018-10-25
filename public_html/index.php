1274a72cb5bd343b8374a8265e7e5b4c60caef8f
Christian Westbrook <christianwestbrook@live.com>
2018-10-25 15:42:02 -0500

    Updated script in index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,17 @@
+<head>
+<script type="text/javascript">
+	function submit()
+	{
+		var input = document.getElementById("input").value;
+	
+		if(input !== null)
+		{
+			document.write(input);
+		}
+	}
+	</script>
+</head>
+
 <form onsubmit="submit()" method="post">
 	Input:<br>
 	<input type="text" id="input"><br>
@@ -6,15 +20,6 @@
 	<input type="submit" value="Send">	
 </form>
 
-function submit()
-{
-	var input = document.getElementById("input").value;
-	
-	if(input !== null)
-	{
-		document.write(input);
-	}
-}
 
 
 
