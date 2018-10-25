4dbaeb4f821b2b20e9c8cd4baa7234e4cf31f8cd
Christian Westbrook <christianwestbrook@live.com>
2018-10-25 15:36:24 -0500

    Bug fix in index.php

--- public_html/index.php
+++ public_html/index.php
@@ -2,18 +2,22 @@
 
 ?>
 
-<script type="text/javascript">
-	var input = document.getElementById("input");
-	document.write(input);
-</script>
-
-<form onsubmit="submit()"/server.php" method="post">
+<form onsubmit="submit()" method="post">
 	Input:<br>
 	<input type="text" id="input"><br>
 	<input type="checkbox" name="NB" value="NB" checked> Naive Bayes<br>
 	<input type="checkbox" name="LR" value="LR"> Logistic Regression<br>
 	<input type="submit" value="Send">	
 </form>
+
+<script type="text/javascript">
+	var input = document.getElementById("input");
+	
+	if(input !== null)
+	{
+		document.write(input);
+	}
+</script>
 
 
 
