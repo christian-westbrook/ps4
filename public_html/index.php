a063069597aa9f888e403bbc36d0d74811d849df
Christian Westbrook <christianwestbrook@live.com>
2018-10-25 15:17:42 -0500

    Updated index.php

--- public_html/index.php
+++ public_html/index.php
@@ -5,5 +5,10 @@
 <form>
 	Input:<br>
 	<input type="text"><br>
+	<input type="checkbox" name="NB" value="NB" checked> Naive Bayes<br>
+	<input type="checkbox" name="LR" value="LR"> Logistic Regression<br>
 	<input type="submit" value="Send">
 </form>
+
+
+
