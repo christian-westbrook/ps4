b410e8ad5bdedaf688b556e8d0be830327d7f01a
Christian Westbrook <christianwestbrook@live.com>
2018-10-25 17:20:58 -0500

    Update server.php

--- public_html/server.php
+++ public_html/server.php
@@ -3,9 +3,10 @@ $address = "127.0.0.1";
 $port = "5000";
 $in = $_POST['input'];
 
+echo "HERE";
+
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 $result = socket_connect($socket, $address, $port);
-echo "HERE";
 socket_write($socket, $in  . "\r\n", strlen($in . "\r\n"));
 socket_close($socket);
 
