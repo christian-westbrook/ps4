f092989947168a566071fcb97b4dbde5b9cabda0
Christian Westbrook <christianwestbrook@live.com>
2018-10-25 15:52:02 -0500

    Updated index.php

--- public_html/index.php
+++ public_html/index.php
@@ -3,11 +3,11 @@
 <script src="./js/submit-event.js"></script>	
 </head>
 
-<form action="#" method="post" onsubmit="return submit()">
+<form action="#" method="post" onsubmit="return submit();">
 	Input:<br>
-	<input type="text" id="input" value=""><br>
-	<input type="checkbox" name="NB" value="NB" checked> Naive Bayes<br>
-	<input type="checkbox" name="LR" value="LR"> Logistic Regression<br>
+	<input type="text"><br>
+	<input type="checkbox" name="NB" checked> Naive Bayes<br>
+	<input type="checkbox" name="LR"> Logistic Regression<br>
 	<input type="submit" value="Send">	
 </form>
 
