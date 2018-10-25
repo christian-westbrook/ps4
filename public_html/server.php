025a3c9db01897f93509edc134a088d981061f22
Christian Westbrook <christianwestbrook@live.com>
2018-10-25 17:05:48 -0500

    Update server.php

--- public_html/server.php
+++ public_html/server.php
@@ -6,7 +6,7 @@ $in = $_POST['input'];
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 $clientConnection = socket_connect($socket, $address, $port);
 socket_write($clientConnection, $in, strlen($in));
-socket_close($socket);
+socket_close($clientConnection);
 
 header('index.php');
 ?>
