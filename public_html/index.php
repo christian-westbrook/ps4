767fb50ac02550e81d4d5bf3f77c165c60af0c3c
Christian Westbrook <christianwestbrook@live.com>
2018-10-25 15:59:09 -0500

    Updated index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,16 +1,17 @@
 <head>
 <title>PS4</title>
-<script src="./js/submit-event.js"></script>	
+<script src="js/submit-event.js"></script>	
 </head>
 
-<form action="#" method="post" onsubmit="return submitEvent();">
+<body>
+<form action="#" method="post" onsubmit="submitEvent();">
 	Input:<br>
 	<input type="text"><br>
 	<input type="checkbox" name="NB" checked> Naive Bayes<br>
 	<input type="checkbox" name="LR"> Logistic Regression<br>
 	<input type="submit" value="Send">	
 </form>
-
+</body>
 
 
 
