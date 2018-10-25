1528dceb2976a79b4cdf215730972405e6a8eb6a
Christian Westbrook <christianwestbrook@live.com>
2018-10-25 16:27:37 -0500

    Updated server.php
    Added socket code

--- public_html/server.php
+++ public_html/server.php
@@ -1,3 +1,9 @@
 <?php
-echo $_POST['input'];
+$address = "localhost";
+$port = "5000";
+$in = $_POST['input'];
+
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+$clientConnection = socket_connect($socket, $address, $port);
+socket_write($socket, $in, strlen($in));
 ?>
