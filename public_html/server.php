dddb9f3964a8daf415490e5d680f3c87dfec3d6d
Christian Westbrook <christianwestbrook@live.com>
2018-10-25 17:08:30 -0500

    Updated server.php

--- public_html/server.php
+++ public_html/server.php
@@ -4,9 +4,9 @@ $port = "5000";
 $in = $_POST['input'];
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-$clientConnection = socket_connect($socket, $address, $port);
-socket_write($clientConnection, $in, strlen($in));
-socket_close($clientConnection);
+socket_bind($socket, $address, $port);
+socket_write($socket, $in, strlen($in));
+socket_close($socket);
 
 header('index.php');
 ?>
