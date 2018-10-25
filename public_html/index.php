dd7641c65619be259641dee1a2eebc326e906f28
Christian Westbrook <christianwestbrook@live.com>
2018-10-25 15:33:24 -0500

    Test code in index.php

--- public_html/index.php
+++ public_html/index.php
@@ -2,12 +2,17 @@
 
 ?>
 
-<form>
+<script type="text/javascript">
+	var input = document.getElementById("input");
+	document.write(input);
+</script>
+
+<form onsubmit="submit()"/server.php" method="post">
 	Input:<br>
-	<input type="text"><br>
+	<input type="text" id="input"><br>
 	<input type="checkbox" name="NB" value="NB" checked> Naive Bayes<br>
 	<input type="checkbox" name="LR" value="LR"> Logistic Regression<br>
-	<input type="submit" value="Send">
+	<input type="submit" value="Send">	
 </form>
 
 
