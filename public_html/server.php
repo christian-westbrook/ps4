9aa9d7aa47d675484b4a300d7dc41f8dde5fe330
Christian Westbrook <christianwestbrook@live.com>
2018-10-28 20:26:28 -0500

    Updated server.php

--- public_html/server.php
+++ public_html/server.php
@@ -3,14 +3,28 @@
 // Tell the compiler to print any errors that occur
 error_reporting(E_ALL);
 
+// Notify the administrator that the server class is executing
+echo "[Status] Initializing";
+
 // Get the message to be sent
 $in = $_POST['input'];
+
+// Notify the administrator that the input message has been received
+echo "[Status] Input received";
+echo "         Input: " + $in;
 
 // Get the IP address by host name
 $address = gethostbyname("code.cis.uafs.edu");
 
+// Notify the administrator that the host name has been retrieved
+echo "[Status] Retrieved host";
+echo "         Host: " + $address;
+
 // Set the port for communication
 $port = "5000";
+
+// Notify the administrator that the client socket is being created
+echo "[Status] Creating client socket";
 
 // Create the client socket
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
@@ -22,6 +36,9 @@ if($socket === false)
 	echo socket_strerror(socket_last_error()) . "\n";
 }
 
+// Notify the administrator that the client is attempting to connect to the server
+echo "[Status] Attempting connection to server";
+
 // Attempt to connect to the server with the client socket
 $result = socket_connect($socket, $address, $port);
 
@@ -32,13 +49,22 @@ if($result === false)
 	echo socket_strerror(socket_last_error($socket)) . "\n";
 }
 
+// Notify the administrator that the client is writing a message to the server
+echo "[Status] Sending message to server connection";
+
 // Append end of message to input message
 $in = $in . "\r\n\r\n";
 
 // Write message to server connection
 socket_write($socket, $in, strlen($in));
 
+// Notify the administrator that the connection is being closed
+echo "[Status] Closing connection";
+
 // Close the socket
 socket_close($socket);
+
+// Notify the administrator that the program is terminating
+echo "[Status] Terminating";
 
 ?>
