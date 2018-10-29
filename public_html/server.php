35d003a7516e721a979c69f0e71bdd95bbe81cde
Christian Westbrook <christianwestbrook@live.com>
2018-10-29 00:07:59 -0500

    Updated test code in server

--- public_html/server.php
+++ public_html/server.php
@@ -27,16 +27,13 @@ $port = "5000";
 echo "[Status] Creating client socket\n";
 
 // Create the client socket
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-
-echo "HERE";
-
-// Test the client socket
-if($socket === false)
+if ($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) === false)
 {
 	echo "socket_create() failed.\n";
 	echo socket_strerror(socket_last_error()) . "\n";
 }
+
+echo "HERE";
 
 // Notify the administrator that the client is attempting to connect to the server
 echo "[Status] Attempting connection to server\n";
