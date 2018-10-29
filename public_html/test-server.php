1a1fa38b4f0e77eb732e0da1f3bd7d02953e2b6a
Christian Westbrook <christianwestbrook@live.com>
2018-10-29 01:31:47 -0500

    Updated status messages in server.php

--- public_html/test-server.php
+++ public_html/test-server.php
@@ -4,32 +4,32 @@
 error_reporting(E_ALL);
 
 // Notify the administrator that the server class is executing
-echo "[Status] Initializing\n";
+echo "[Status] Initializing</br>";
 
 // Get the message to be sent
 $in = $_POST['input'];
 
 // Notify the administrator that the input message has been received
-echo "[Status] Input received\n";
-echo "         Input: " . $in . "\n";
+echo "[Status] Input received</br>";
+echo "         Input: " . $in . "</br>";
 
 // Get the IP address by host name
 $address = gethostbyname("code.cis.uafs.edu");
 
 // Notify the administrator that the host name has been retrieved
-echo "[Status] Retrieved host\n";
-echo "         Host: " . $address . "\n";
+echo "[Status] Retrieved host</br>";
+echo "         Host: " . $address . "</br>";
 
 // Set the port for communication
 $port = "5000";
 
 // Notify the administrator that the client socket is being created
-echo "[Status] Creating client socket\n";
+echo "[Status] Creating client socket</br>";
 
 // Create the client socket
 $socket = stream_socket_client("tcp:" . $address . ":" . $port, $errno, $errstr, 30);
 
-echo "HERE";
+echo "HERE</br>";
 
 // Test the client socket
 if(!$socket)
@@ -38,7 +38,7 @@ if(!$socket)
 }
 
 // Notify the administrator that the client is writing a message to the server
-echo "[Status] Sending message to server connection\n";
+echo "[Status] Sending message to server connection</br>";
 
 // Append end of message to input message
 $in = $in . "\r\n";
@@ -47,7 +47,7 @@ $in = $in . "\r\n";
 fwrite($socket, $in);
 
 // Notify the administrator that the connection is being closed
-echo "[Status] Closing connection\n";
+echo "[Status] Closing connection</br>";
 
 // Close the socket
 fclose($socket);
