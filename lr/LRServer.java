4b126835591a35f6e4f886874cffeb95f9306b04
Christian Westbrook <christianwestbrook@live.com>
2018-10-31 01:34:35 -0500

    Added client/server code to LR

--- lr/LRServer.java
+++ lr/LRServer.java
@@ -1,1 +1,105 @@
 package lr;
+
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+
+public class LRServer
+{
+	public LRServer()
+	{	
+		// Port to listen on
+		int port = 5001;
+		
+		
+		try
+		{
+			// Create a server socket that will listen at a specified port number
+			ServerSocket serverSocket = new ServerSocket(port);
+		
+	
+		// While listening
+		while(true)
+		{
+			try
+			{
+					// Notify administrator that the server is listening
+					System.out.println("[Status] Listening on port " + serverSocket.getLocalPort());
+			
+					// Create a client socket connection by accepting an incoming connection to the server socket
+					Socket clientSocket = serverSocket.accept();
+			
+					// Notify the administrator that a connection has been accepted
+					System.out.println("[Status] Accepted client connection at " + clientSocket.getLocalPort());
+			
+					// Define inClient as the inputStream of the client socket
+					BufferedReader inClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+					
+					// Define outClient as the outputStream of the client socket
+					PrintWriter outClient = new PrintWriter(clientSocket.getOutputStream(), true);
+			
+					// Notify the administrator that the server is reading input from the client
+					System.out.println("[Status] Receiving input from the client connection");
+			
+					// Read one line of input from the client connection
+					String input = inClient.readLine();
+			
+					// Notify the administrator that the message was received
+					System.out.println("[Status] Message received: " + input);
+				
+					// Notify the administrator that the naive Bayes algorithm is calculating sentiment
+					System.out.println("[Status] Calculating sentiment");
+				
+					// Create sentence transfer object
+					STO inSTO = new STO();
+					inSTO.setInput(input);
+				
+					// Calculate sentiment
+					LR logisticRegression = new LR();
+					STO outSTO = logisticRegression.calc(inSTO);
+				
+					// Notify the administrator that the server is writing back to the client
+					System.out.println("[Status] Writing response to server");
+				
+					// Format results
+					String pos = Double.toString(outSTO.getPos());
+					String neu = Double.toString(outSTO.getNeu());
+					String neg = Double.toString(outSTO.getNeg());
+					String output = outSTO.getClassifier() + "," + pos + "," + neu + "," + neg + ",\r\n";
+				
+					// Write the output back to the client
+					outClient.println(output);
+				
+					// Notify the administrator that the server is closing the connection
+					System.out.println("[Status] Closing connection");
+				
+					// Close the client connection
+					inClient.close();
+					outClient.close();
+					clientSocket.close();
+				}
+				catch(ConnectException ex)
+				{
+					ex.printStackTrace();
+				}
+				catch(IOException ex)
+				{
+					ex.printStackTrace();
+				}
+			}
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		new LRServer();
+	}
+}
--- public_html/client.php
+++ public_html/client.php
@@ -3,30 +3,70 @@
 // Get the message to be sent
 $in = $_POST['input'];
 
-// Create the client socket
-$socket = stream_socket_client("tcp://code.cis.uafs.edu:5000", $errno, $errstr, 30);
+// Detect options
+$nb = $_POST['NB'];
+$lr = $_POST['LR'];
 
-// Test the client socket
-if(!$socket)
+// TEST
+echo $nb;
+echo $lr;
+
+if($nb)
 {
-	echo "$errstr ($errno)<br />\n";
+	// Create the client socket
+	$socket = stream_socket_client("tcp://code.cis.uafs.edu:5000", $errno, $errstr, 30);
+
+	// Test the client socket
+	if(!$socket)
+	{
+		echo "$errstr ($errno)<br />\n";
+	}
+
+	// Append end of message to input message
+	$in = $in . "\n";
+
+	// Write message to server connection
+	fwrite($socket, $in);
+
+	// Receive message from server connection
+	echo "Response: ";
+	while (!feof($socket)) 
+	{
+		echo fgets($socket, 4096);
+	}
+	echo "</br>";
+
+	// Close the socket
+	fclose($socket);
 }
 
-// Append end of message to input message
-$in = $in . "\n";
-
-// Write message to server connection
-fwrite($socket, $in);
-
-// Receive message from server connection
-echo "Response: ";
-while (!feof($socket)) 
+if($lr)
 {
-    echo fgets($socket, 4096);
-}
-echo "</br>";
+	// Create the client socket
+	$socket = stream_socket_client("tcp://code.cis.uafs.edu:5001", $errno, $errstr, 30);
 
-// Close the socket
-fclose($socket);
+	// Test the client socket
+	if(!$socket)
+	{
+		echo "$errstr ($errno)<br />\n";
+	}
+
+	// Append end of message to input message
+	$in = $in . "\n";
+
+	// Write message to server connection
+	fwrite($socket, $in);
+
+	// Receive message from server connection
+	echo "Response: ";
+	while (!feof($socket)) 
+	{
+		echo fgets($socket, 4096);
+	}
+	echo "</br>";
+
+	// Close the socket
+	fclose($socket);
+}
 
 ?>
